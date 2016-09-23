1ae00e3b66be6a30f7e71e19065488782b2b7aca
GlennGoossens <glenngoossens07@gmail.com>
2016-09-23 14:58:40 +0200

    naamwijziging

---
+++ app/src/main/java/com/goossens/glenn/a6weekfitnessworkout/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.goossens.glenn.a6weekfitnessworkout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
